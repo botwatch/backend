5208732d3f342bcc9d617dfafebe6c311e9e7010
TimD <timd@siqinc.com>
2020-02-11 13:46:35 -0500

    version incr from 3.0 to 3.1

--- botwat.ch/Program.cs
+++ botwat.ch/Program.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using InternalSite;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+
 
 namespace botwat.ch
 {
--- botwat.ch/Startup.cs
+++ botwat.ch/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace InternalSite
+namespace botwat.ch
 {
     public class Startup
     {
