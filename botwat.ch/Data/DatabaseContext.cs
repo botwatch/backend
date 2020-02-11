f7d8608d0a123fd89c42a8e1401a7010b96f7c46
TimD <timd@siqinc.com>
2020-02-11 16:26:33 -0500

    Updated Connection String to pull from a byte array.

--- botwat.ch/Data/DatabaseContext.cs
+++ botwat.ch/Data/DatabaseContext.cs
@@ -12,7 +12,7 @@ namespace botwat.ch.Data
         {
             var connectionString = Startup.Configuration.GetConnectionString("Master-Database");
             connectionString += Encoding.UTF8.GetString(
-                new byte[] {46, 69, 72, 65, 53, 0x6e, 61, 69, 0x6c, 36, 37, 38, 21, 0x3b}
+                new byte[] {70, 105, 114, 101, 83 ,110, 97 ,105, 108, 54, 55, 56, 33 ,59}
             );
             optionsBuilder.UseMySql(
                 connectionString
