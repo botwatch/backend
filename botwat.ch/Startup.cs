993f5b533f67fc2def76737f4ad31a0f43b4060c
TimD <timd@siqinc.com>
2020-02-12 08:54:04 -0500

    Updated launch port and favicon.ico

--- botwat.ch/Startup.cs
+++ botwat.ch/Startup.cs
@@ -32,7 +32,6 @@ namespace botwat.ch
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
             DbProvider.Context.Database.EnsureCreated();
             if (env.IsDevelopment())
             {
@@ -46,6 +45,7 @@ namespace botwat.ch
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
