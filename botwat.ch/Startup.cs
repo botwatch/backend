2570e28b5d68fe0d93ef1881301f4621cdbd08a3
TimD <timd@siqinc.com>
2020-02-15 10:14:36 -0500

    Uncomment needed code

--- botwat.ch/Startup.cs
+++ botwat.ch/Startup.cs
@@ -28,7 +28,7 @@ namespace botwat.ch
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //if (!Debugger.IsAttached)  services.AddLetsEncrypt();
+            services.AddLetsEncrypt();
             services.AddControllersWithViews();
             services.AddControllers().AddNewtonsoftJson(x =>
             {
