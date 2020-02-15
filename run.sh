d6f137b913dcdae683b79f2c2d2df9e699ddfb79
Tim Dekker <timdeka@gmail.com>
2020-02-15 11:46:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git pull
 dotnet publish -c Release --output ./production
 cd production
 kill $(ps aux | grep 'botwat.ch.dll' | awk '{print $2}')
-dotnet ./botwat.ch.dll &
+dotnet ./botwat.ch.dll & 
