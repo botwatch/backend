1112d744b48015ca8b0f613fcb35c1663e2c48ee
TimD <timd@siqinc.com>
2020-03-29 17:33:24 -0400

    remove existing workflows

--- botwat.ch/Web/src/pages/Accounts.tsx
+++ botwat.ch/Web/src/pages/Accounts.tsx
@@ -1,44 +1,113 @@
 import {
-    Container,
-    CssBaseline, Grid, makeStyles
+    makeStyles,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow
 } from "@material-ui/core";
 import React, {useEffect} from "react";
 import {authenticationService} from "../services/authentication.service";
 import {accountService} from "../services/account.service";
-import {IClient} from "../data/dto/client/IClient";
-import AccountCard from "../components/AccountCard";
 import {IOldSchoolAccount} from "../data/dto/account/IOldSchoolAccount";
 
 const useStyles = makeStyles({
     root: {
-        paddingTop: 10,
-        backgroundColor: '#f5f5f5'
-    }
+        width: '100%',
+    },
+    container: {
+        maxHeight: 440,
+    },
 });
 
+interface Column {
+    id: 'name' | 'time' | 'exp' | 'sessions' | 'bantime';
+    label: string;
+    minWidth?: number;
+    align?: 'right';
+    format?: (value: number) => string;
+}
+
+const columns: Column[] = [
+    {id: 'name', label: 'Name', minWidth: 170},
+   // {id: 'time', label: 'Time', minWidth: 100},
+   // {id: 'exp', label: 'Experience', minWidth: 170,},
+   // {id: 'sessions', label: 'Sessions', minWidth: 170},
+    {id: 'bantime', label: 'Banned', minWidth: 170}
+];
 export default function Accounts() {
-    const classes = useStyles({});
-    const [accounts, setAccounts] = React.useState<string | IOldSchoolAccount[]>("NONE");
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [accounts, setAccounts] = React.useState<IOldSchoolAccount[]>([] as IOldSchoolAccount[]);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     useEffect(() => {
         let user = authenticationService.currentUserValue;
         if (user != null) {
             if (user.token != null) {
                 authenticationService.login(user.name, user.token).then(user => {
-                    accountService.getAccounts().then(a => setAccounts(a));                    
+                    accountService.getAccounts().then(a => setAccounts(a));
                 });
             }
         }
     }, []);
 
     return (
-        <div className={classes.root}>
-            <Grid container spacing={2}>
-                {
-                    typeof accounts !== "string" ? accounts?.map((acc) => <AccountCard account={acc}/>) :
-                        <p>none</p>
-                }
-            </Grid>
-        </div>
+        <Paper className={classes.root}>
+            <TableContainer className={classes.container}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map(column => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{minWidth: column.minWidth}}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {accounts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                            return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    {columns.map(column => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell key={column.id} align={column.align}>
+                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={accounts.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
     );
 }
