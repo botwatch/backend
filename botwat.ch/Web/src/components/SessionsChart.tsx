04e5fe73b566cd1de5677d36e57df16969423179
TimD <timd@siqinc.com>
2020-04-01 08:36:44 -0400

    Added chart to Sessions.tsx

---
+++ botwat.ch/Web/src/components/SessionsChart.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/styles';
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    CardActions,
+    Divider,
+    Button
+} from '@material-ui/core';
+import {
+    Chart,
+    BarSeries,
+    Title,
+    ArgumentAxis,
+    ValueAxis,
+} from '@devexpress/dx-react-chart-material-ui';
+import {Animation} from '@devexpress/dx-react-chart';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import {ISession} from "../data/dto/session/ISession";
+import moment from "moment";
+import {Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {width: '100%'},
+    chartContainer: {
+        height: 400
+    },
+    actions: {
+        justifyContent: 'flex-end'
+    }
+}));
+
+interface Count {
+    date: string,
+    count: number
+}
+
+
+export default function SessionsChart({sessions}) {
+    const classes = useStyles();
+
+    let count: Count[] = [];
+    for (let i = 0; i < 7; i++) count.push({date: "", count: 0} as Count);
+    let localSessions = sessions as ISession[];
+    let weeklySessions = localSessions.filter(session => moment(session.start).isBetween(
+        moment().startOf('day').subtract(1, 'week'), moment()
+    ));
+    if (weeklySessions.length == 0) return <p>No Sessions...</p>;
+
+    weeklySessions.forEach(session => {
+        for (let i = 0; i < 7; i++) {
+            let day = moment().subtract(i, 'days');
+            let index = 6 - i;
+            count[index].date = i == 0 ? "Today" : moment().subtract(i, 'days').format('dddd');
+            if (moment(session.start).isSame(day, 'day')) {
+                count[index].count++;
+            }
+        }
+    });
+
+    console.info(count);
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                action={
+                    <Button
+                        size="small"
+                        variant="text"
+                    >
+                        Last 7 days <ArrowDropDownIcon/>
+                    </Button>
+                }
+                title="This Week"
+            />
+            <Divider/>
+            <CardContent>
+                <Chart data={count}>
+                    <ArgumentAxis/>
+                    <ValueAxis />
+                    <BarSeries
+                        valueField="count"
+                        argumentField="date"
+                        color={'#3F51B5'}
+                    />
+                    <Title text="Sessions run"/>
+                    <Animation/>
+                </Chart>
+            </CardContent>
+            <Divider/>
+            <CardActions className={classes.actions}>
+                <Button
+                    color="primary"
+                    size="small"
+                    variant="text"
+                >
+                    Overview <ArrowRightIcon/>
+                </Button>
+            </CardActions>
+        </Card>
+    );
+};
--- botwat.ch/Web/src/pages/Sessions.tsx
+++ botwat.ch/Web/src/pages/Sessions.tsx
@@ -1,15 +1,17 @@
 import {
     Container,
-    CssBaseline, Grid, makeStyles
+    CssBaseline, Grid, makeStyles, Paper
 } from "@material-ui/core";
 import React, {useEffect} from "react";
 import {authenticationService} from "../services/authentication.service";
 import {accountService} from "../services/account.service";
 import {ISession} from "../data/dto/session/ISession";
 import SessionCard from "../components/SessionCard";
+import SessionsChart from "../components/SessionsChart";
 
 const useStyles = makeStyles({
     root: {
+        width: '100%',
         paddingTop: 10,
         backgroundColor: '#f5f5f5'
     }
@@ -17,7 +19,7 @@ const useStyles = makeStyles({
 
 export default function Sessions() {
     const classes = useStyles({});
-    const [sessions, setSessions] = React.useState<string | ISession[]>("NONE");
+    const [sessions, setSessions] = React.useState<ISession[]>([]);
 
     useEffect(() => {
         (async function anyNameFunction() {
@@ -25,31 +27,32 @@ export default function Sessions() {
             if (user != null) {
                 if (user.token != null) {
                     if (await authenticationService.login(user.name, user.token) != null) {
-                        let localSessions: ISession[] = await accountService.getSessions();
-                        if (typeof localSessions !== "string") {
-                            let ids = localSessions.flatMap((s) => s.id);
-
-                            let localInteractions = await accountService.getInteractions(ids);
-                            if (typeof localInteractions !== "string") {
-                                localInteractions.forEach(act => {
-                                    let id = act[0].id;
-                                    let session = localSessions.find(sess => sess.id == id);
-                                    if (session != undefined)
-                                        session.actions = act;
-                                });
-
-                                let localexperiences = await accountService.getExperiences(ids);
-                                if (typeof localexperiences !== "string") {
-                                    localexperiences.forEach(exp => {
-                                        let id = exp[0].id;
-                                        let session = localSessions.find(sess => sess.id == id);
-                                        if (session != undefined)
-                                            session.experiences = exp;
-                                    });
-                                }
-                                setSessions(localSessions);
-                            }
-                        }
+                        let localSessions: any = await accountService.getSessions();
+                        setSessions(localSessions);
+                        /* if (typeof localSessions !== "string") {
+                             let ids = localSessions.flatMap((s) => s.id);
+ 
+                             let localInteractions = await accountService.getInteractions(ids);
+                             if (typeof localInteractions !== "string") {
+                                 localInteractions.forEach(act => {
+                                     let id = act[0].id;
+                                     let session = localSessions.find(sess => sess.id == id);
+                                     if (session != undefined)
+                                         session.actions = act;
+                                 });
+ 
+                                 let localexperiences = await accountService.getExperiences(ids);
+                                 if (typeof localexperiences !== "string") {
+                                     localexperiences.forEach(exp => {
+                                         let id = exp[0].id;
+                                         let session = localSessions.find(sess => sess.id == id);
+                                         if (session != undefined)
+                                             session.experiences = exp;
+                                     });
+                                 }
+                                 setSessions(localSessions);
+                             }
+                         }*/
                     }
                 }
             }
@@ -57,14 +60,10 @@ export default function Sessions() {
     }, []);
 
     return (
-        <div className={classes.root}>
-            <Grid container spacing={2}>
-                {
-                    typeof sessions !== "string" ?
-                        sessions?.map((session) => <SessionCard session={session}/>)
-                        : <p>none</p>
-                }
+        <Paper className={classes.root}>
+            <Grid container spacing={6}>
+                <SessionsChart sessions={sessions}/>
             </Grid>
-        </div>
+        </Paper>
     );
 }
